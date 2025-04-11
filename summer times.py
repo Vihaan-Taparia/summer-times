f693278e96f5e59d1aed9615bf70314668ad6e04
Vihaan-Taparia <mayataparia@gmail.com>
2025-04-11 21:26:02 +0530

    Add files via upload

---
+++ summer times.py
@@ -0,0 +1,7 @@
+temp=int(input("What is the current Temperature......."))
+
+if temp>35:
+    print("This is the suitable temperature for light clothing")
+
+else:
+    print("You can wear dark clothing without getting too hot")
